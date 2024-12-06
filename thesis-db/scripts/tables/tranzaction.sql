e032870c51d2a512f817576fef286930621a659e
Balint Toth <t.balint.k@gmail.com>
2024-12-06 07:30:42 +0000

    Implementing currency conversion

--- thesis-db/scripts/tables/tranzaction.sql
+++ thesis-db/scripts/tables/tranzaction.sql
@@ -6,6 +6,7 @@ create table tranzaction
 	instrument_fk int not null,
 	amount numeric(14, 4) not null,
 	price numeric(14, 4) not null,
+	rate numeric(14, 4) not null,
 	
 	constraint fk1 foreign key (portfolio_fk) references portfolio (id) ON DELETE CASCADE,
 	constraint fk2 foreign key (instrument_fk)  references instrument (id)
